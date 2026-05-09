69f114e23dbaa2d1499122586263d6f1ece0651f
lamanonim <lmanmustafayeva0@email.com>
2026-05-09 18:01:51 +0000

    Add Student class with filtered to_json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines Student class."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of Student."""
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
