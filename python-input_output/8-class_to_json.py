ec6887622044727b44a9c5fbc7f4ac85ea767145
lamanonim <lmanmustafayeva0@email.com>
2026-05-09 17:59:24 +0000

    Add class_to_json function

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines class_to_json function."""
+
+
+def class_to_json(obj):
+    """Returns dictionary description for JSON serialization of an object."""
+    return obj.__dict__
