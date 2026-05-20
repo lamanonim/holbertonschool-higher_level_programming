1d927191945308ee4ce395296680b811beb924ff
lamanonim <lmanmustafayeva0@gmail.com>
2026-05-20 22:32:45 +0000

    python-network_1: fix task 1 - update docstring

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that takes in a URL, sends a request and displays X-Request-Id"""
+"""This script takes a URL, sends a request, and displays the X-Request-Id header value."""
 import urllib.request
 import sys
 
