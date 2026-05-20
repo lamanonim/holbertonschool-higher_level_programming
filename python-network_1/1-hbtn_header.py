2b7cf4f03ccecce5eb4a99e54c903148b5c52734
lamanonim <lmanmustafayeva0@gmail.com>
2026-05-20 22:33:36 +0000

    python-network_1: fix task 1 - multiline docstring

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""This script takes a URL, sends a request, and displays the X-Request-Id header value."""
+"""
+This script takes a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
 import urllib.request
 import sys
 
